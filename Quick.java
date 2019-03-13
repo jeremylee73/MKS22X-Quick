58680264c352bcc1b279be0b8722f86bb10c1e27
Jeremy Lee <jlee00@stuy.edu>
2019-03-13 14:48:03 -0400

    Fixing bug on partition

--- Quick.java
+++ Quick.java
@@ -1,10 +1,6 @@
 import java.util.*;
 
 public class Quick{
-  private static int pivot;
-  private static int pivotIndex;
-  private static int s;
-  private static int e;
     /*Modify the array such that:
    *1. Only the indices from start to end inclusive are considered in range
    *2. A random index from start to end inclusive is chosen, the corresponding
@@ -14,10 +10,8 @@ public class Quick{
    *@return the index of the final position of the pivot element.
    */
   public static int partition(int[] data, int start, int end){
-    s = start;
-    e = end;
-    pivotIndex = (int)(Math.random() * data.length);
-    pivot = data[pivotIndex];
+    int pivotIndex = (int)(Math.random() * data.length);
+    int pivot = data[pivotIndex];
     swap(data, start, pivotIndex);
     while (start != end){
       if (data[start] > pivot) {
@@ -28,17 +22,13 @@ public class Quick{
       }
     }
 
-    if (data[data.length-1] <= pivot){
-      swap(data, 0, data.length-1);
+    if (pivot >= start){
+      swap(data, pivotIndex, start);
+      return start;
+    } else {
+      swap(data, pivotIndex, start-1);
+      return start-1;
     }
-
-    for (int i=0; i<data.length-1; i++){
-      if (data[i] <= pivot && data[i+1] >= pivot){
-        swap(data, 0, i);
-        pivotIndex = i;
-      }
-    }
-    return pivot;
   }
 
   private static void swap(int[] data, int a, int b){
@@ -48,39 +38,53 @@ public class Quick{
   }
 
   /*return the value that is the kth smallest value of the array. k=0 is the smallest*/
-  public static int quickselect(int[] data, int k){
-    if (k == partition(data,0,data.length-1)){
-      return data[pivotIndex];
+  // public static int quickselect(int[] data, int k){
+  //   if (k == partition(data,0,data.length-1)){
+  //     return data[pivotIndex];
+  //   }
+  //   if (k > partition(data, 0, data.length-1)){
+  //     int[] newData = new int[e-pivotIndex];
+  //     for (int i=0; i<newData.length; i++){
+  //       newData[i] = data[i+pivotIndex];
+  //     }
+  //     return quickselect(newData, k);
+  //   }
+  //   if (k < partition(data, 0, data.length-1)){
+  //     int[] newData = new int[pivotIndex+1];
+  //     for (int i=0; i<newData.length; i++){
+  //       newData[i] = data[i];
+  //     }
+  //     return quickselect(newData, k);
+  //   }
+  //   return 0;
+  // }
+
+  public static void quicksort(int[] data, int lo, int hi){
+    if (lo >= hi){
+      return;
     }
-    if (k > partition(data, 0, data.length-1)){
-      int[] newData = new int[e-pivotIndex];
-      for (int i=0; i<newData.length; i++){
-        newData[i] = data[i+pivotIndex];
-      }
-      return quickselect(newData, k);
+    int pivot = partition(data, lo, hi);
+    System.out.println("Pivot #:  "+data[pivot]);
+    for (int i=0; i<data.length; i++){
+      System.out.print(data[i] + " ");
     }
-    if (k < partition(data, 0, data.length-1)){
-      int[] newData = new int[pivotIndex+1];
-      for (int i=0; i<newData.length; i++){
-        newData[i] = data[i];
-      }
-      return quickselect(newData, k);
-    }
-    return 0;
+    System.out.println();
+    // for (int i=lo; i<=hi; i++){
+    //   System.out.print(data[i] + " ");
+    // }
+    // System.out.println();
+    quicksort(data, lo, pivot-1);
+    quicksort(data, pivot+1, hi);
   }
 
   public static void main(String[] args){
-    int[] test = new int[] {17,61,67,47,93,12,20,4,44,68};
+    int[] test = {17,61,67,47,93,12,20,4,44,68};
     for (int i=0; i<test.length; i++){
       System.out.print(test[i] + " ");
     }
     System.out.println();
-    System.out.println(partition(test,0,test.length-1));
-    for (int i=0; i<test.length; i++){
-      System.out.print(test[i] + " ");
-    }
-    System.out.println();
-    System.out.println(quickselect(test, 4));
+
+    System.out.println(test[partition(test,0,test.length-1)]);
     for (int i=0; i<test.length; i++){
       System.out.print(test[i] + " ");
     }
